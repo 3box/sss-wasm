24e1c8c98d83a803e6560d2455749f77f0499983
Sergey Ukustov <sergey@ukstv.me>
2022-07-26 18:45:00 +0100

    wip

---
+++ src/lib.js
@@ -0,0 +1,102 @@
+globalThis.__dirname = import.meta.url.match(/^(.+)\/([^\/]+)$/)[1].replace(/^file:\/\//, '')
+import { createRequire } from 'module';
+globalThis.require = (p) => {
+  const origRequire = createRequire(import.meta.url);
+  if (p === 'path') {
+    const module = origRequire('path')
+    return {
+      ...module,
+      normalize: (a) => a.replace(/^file:\/\//, '')
+    }
+  } else {
+    return origRequire(p)
+  }
+}
+
+import Wrapper from "./gen/wrapper.js";
+
+let api = undefined;
+
+function load() {
+  if (api) return new Promise(resolve => resolve(api));
+  return new Promise(resolve => {
+    Wrapper().then(Module => {
+      api = {
+        set: (d, p) => Module.HEAPU8.set(d, p),
+        slice: (f, t) => Module.HEAPU8.slice(f, t),
+        getShareLen: Module.cwrap('get_share_len', 'number', []),
+        getMessageLen: Module.cwrap('get_message_len', 'number', []),
+        getKeyshareLen: Module.cwrap('get_keyshare_len', 'number', []),
+        getKeyLen: Module.cwrap('get_key_len', 'number', []),
+        createBuffer: Module.cwrap('create_buffer', 'number', ['number', 'number']),
+        destroyBuffer: Module.cwrap('destroy_buffer', '', ['number']),
+        createShares: Module.cwrap('create_shares', 'number', ['number', 'number', 'number']),
+        combineShares: Module.cwrap('combine_shares', 'number', ['number', 'number']),
+        createKeyshares: Module.cwrap('create_keyshares', 'number', ['number', 'number', 'number']),
+        combineKeyshares: Module.cwrap('combine_keyshares', 'number', ['number', 'number']),
+      };
+      resolve(api);
+    });
+  });
+}
+
+export const createShares = function createShares(data, n, k) {
+  return load().then(api => {
+    const datap = api.createBuffer(data.length);
+    api.set(data, datap);
+    const sharep = api.createShares(datap, n, k);
+    api.destroyBuffer(datap);
+
+    const shareLen = api.getShareLen();
+    const shares = [];
+    for (let i = 0; i < n; i++) {
+      shares[i] = api.slice(sharep + shareLen * i, sharep + shareLen * (i + 1));
+    }
+    return shares;
+  });
+};
+
+export const combineShares = function combineShares(shares) {
+  return load().then(api => {
+    const input = api.createBuffer(api.getShareLen() * shares.length);
+    for (let s in shares) {
+      api.set(shares[s], input + s * api.getShareLen());
+    }
+
+    const datap = api.combineShares(input, shares.length);
+    if (!datap) throw "InvalidAccessError: invalid or too few shares provided";
+    api.destroyBuffer(input);
+
+    return api.slice(datap, datap + api.getMessageLen());
+  });
+};
+
+export const createKeyshares = function createKeyshares(key, n, k) {
+  return load().then(api => {
+    const keyp = api.createBuffer(key.length);
+    api.set(key, keyp);
+    const sharep = api.createKeyshares(keyp, n, k);
+    api.destroyBuffer(keyp);
+
+    const shareLen = api.getKeyshareLen();
+    const shares = [];
+    for (let i = 0; i < n; i++) {
+      shares[i] = api.slice(sharep + shareLen * i, sharep + shareLen * (i + 1));
+    }
+    return shares;
+  });
+}
+
+export const combineKeyshares = function combineKeyshares(keyshares) {
+  return load().then(api => {
+    const input = api.createBuffer(api.getKeyshareLen() * keyshares.length);
+    for (let s in keyshares) {
+      api.set(keyshares[s], input + s * api.getKeyshareLen());
+    }
+
+    const keyp = api.combineKeyshares(input, keyshares.length);
+    api.destroyBuffer(input);
+
+    return api.slice(keyp, keyp + api.getKeyLen());
+  });
+}
---
+++ src/wrapper.c
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+#include <emscripten.h>
+#include "../sss/sss.h"
+#include "../sss/randombytes/randombytes.h"
+
+EMSCRIPTEN_KEEPALIVE
+int get_share_len()
+{
+    return sss_SHARE_LEN;
+}
+
+EMSCRIPTEN_KEEPALIVE
+int get_message_len()
+{
+    return sss_MLEN;
+}
+
+EMSCRIPTEN_KEEPALIVE
+int get_keyshare_len()
+{
+    return sss_KEYSHARE_LEN;
+}
+
+EMSCRIPTEN_KEEPALIVE
+int get_key_len()
+{
+    return 32;
+}
+
+EMSCRIPTEN_KEEPALIVE
+uint8_t *create_shares(uint8_t *data, uint8_t n, uint8_t k)
+{
+    uint8_t *output = (uint8_t *)malloc(sss_SHARE_LEN * n);
+    sss_create_shares((sss_Share *)output, data, n, k);
+    return output;
+}
+
+EMSCRIPTEN_KEEPALIVE
+uint8_t *combine_shares(uint8_t *input, uint8_t k)
+{
+    uint8_t *data = (uint8_t *)malloc(sss_MLEN);
+    if (sss_combine_shares(data, (sss_Share *)input, k) == 0) {
+        return data;
+    } else {
+        free(data);
+        return NULL;
+    }
+}
+
+EMSCRIPTEN_KEEPALIVE
+uint8_t *create_keyshares(uint8_t *key, uint8_t n, uint8_t k)
+{
+    uint8_t *output = (uint8_t *)malloc(sss_KEYSHARE_LEN * n);
+    sss_create_keyshares((sss_Keyshare *)output, key, n, k);
+    return output;
+}
+
+EMSCRIPTEN_KEEPALIVE
+uint8_t *combine_keyshares(uint8_t *input, uint8_t k)
+{
+    uint8_t *key = (uint8_t *)malloc(32);
+    sss_combine_keyshares(key, (sss_Keyshare *)input, k);
+    return key;
+}
+
+EMSCRIPTEN_KEEPALIVE
+uint8_t *create_buffer(uint8_t length)
+{
+    return malloc(length * sizeof(uint8_t));
+}
+
+EMSCRIPTEN_KEEPALIVE
+void destroy_buffer(uint8_t *p)
+{
+    free(p);
+}
